0932c2474e8740c2a652a38de1f915266fedd959
Grigory Sapogov <grisha.sapgv@mail.ru>
2023-12-25 15:32:32 +0500

    Initial Commit

---
+++ Hotel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hotel
+//
+//  Created by Grigory Sapogov on 25.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
